f9b40b2d64e0cdeb71d2c14daabb6643acd5d913
Daniel Estévez <daniel.ef@gmail.com>
2018-03-24 23:58:20 -0600

    Adds cli

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject my-slack-bot "0.1.0-SNAPSHOT"
+(defproject my-slack-bot "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [slack-rtm "0.1.7"]]
   :main my-slack-bot.core)
--- src/my_slack_bot/core.clj
+++ src/my_slack_bot/core.clj
@@ -1,8 +1,8 @@
 (ns my-slack-bot.core
-  (:require [clojure.core.async :refer [go-loop <!]]
-            [clojure.string :refer [lower-case]]
+  (:gen-class)
+  (:require [clojure.string :refer [lower-case]]
+            [clojure.tools.cli :refer [cli]]
             [slack-rtm.core :refer [connect send-event sub-to-event]]))
-
 
 (def dic (read-string (slurp "dic.edn")))
 
@@ -13,5 +13,12 @@
       (send-event dispatcher {:type "message" :text df :channel channel}))))
 
 (defn -main [& args]
-  (let [{:keys [events-publication dispatcher start]} (connect "Bot User OAuth Access Token")]
-    (sub-to-event events-publication :message #(message-receiver dispatcher %))))
+  (let [[{:keys [token]} args banner] (cli args
+                                           ["-h" "--help" "Show help" :flag true :default false]
+                                           ["-t" "--token" "Bot User OAuth Access Token"])]
+    (if token 
+      (let [{:keys [events-publication dispatcher start]} (connect token)]
+        (sub-to-event events-publication :message #(message-receiver dispatcher %)))
+      (do 
+        (println banner)
+        (System/exit 1)))))
