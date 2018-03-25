f3a3543c4a7a7333a502b93a1a1fb6f66fe65980
Daniel Estévez <daniel.ef@gmail.com>
2018-03-25 00:36:38 -0600

    Adds log4j2

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.5"]
+                 [org.apache.logging.log4j/log4j-core "2.8.1"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.8.1"]
                  [slack-rtm "0.1.7"]]
   :main my-slack-bot.core)
--- src/my_slack_bot/core.clj
+++ src/my_slack_bot/core.clj
@@ -2,23 +2,24 @@
   (:gen-class)
   (:require [clojure.string :refer [lower-case]]
             [clojure.tools.cli :refer [cli]]
+            [clojure.tools.logging :as log]
             [slack-rtm.core :refer [connect send-event sub-to-event]]))
 
 (def dic (read-string (slurp "dic.edn")))
 
 (defn message-receiver [dispatcher event]
   (let [{:keys [channel text]} event]
-    (println {:incoming-text event})
+    (log/info {:incoming-text event})
     (if-let [df (dic (lower-case text))]
       (send-event dispatcher {:type "message" :text df :channel channel}))))
 
 (defn -main [& args]
   (let [[{:keys [token]} args banner] (cli args
                                            ["-h" "--help" "Show help" :flag true :default false]
-                                           ["-t" "--token" "Bot User OAuth Access Token"])]
+                                           ["-t" "--token TOKEN" "Bot User OAuth Access Token"])]
     (if token 
       (let [{:keys [events-publication dispatcher start]} (connect token)]
         (sub-to-event events-publication :message #(message-receiver dispatcher %)))
       (do 
-        (println banner)
+        (log/error "Token not specified\n" banner)
         (System/exit 1)))))
